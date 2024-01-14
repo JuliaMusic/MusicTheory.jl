055cfb6719dbab9fbee715dde7b408aae8ea0ffd
David Sanders <dpsanders@gmail.com>
2024-01-14 16:09:48 -0600

    Add scale indexing

--- test/scales.jl
+++ test/scales.jl
@@ -5,4 +5,14 @@
     scale_tones = Base.Iterators.take(scale, 8) |> collect
     @test scale_tones == Pitch[C[4], D[4], E[4], F[4], G[4], A[4], B[4], C[5]]
 
+    @testset "Scale indexing" begin
+        scale = Scale(D[4], major_scale)
+        @test scale[0] == D[4]
+        @test scale[6] == C♯[5]
+        @test scale[7] == D[5]
+        @test scale[-1] == C♯[4]
+        @test scale[-2] == B[3]
+    end
+
+
 end
