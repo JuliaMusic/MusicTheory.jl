e99fdc1740b17b2125796d2abb3fdbb4459060e3
David Sanders <dpsanders@gmail.com>
2024-01-20 16:19:50 -0600

    Fix scale tests

--- test/scales.jl
+++ test/scales.jl
@@ -7,11 +7,11 @@
 
     @testset "Scale indexing" begin
         scale = Scale(D[4], major_scale)
-        @test scale[0] == D[4]
-        @test scale[6] == C♯[5]
-        @test scale[7] == D[5]
-        @test scale[-1] == C♯[4]
-        @test scale[-2] == B[3]
+        @test scale[2] == E[4]
+        @test scale[8] == D[5]
+        @test scale[-1] == D[4]
+        @test scale[-2] == C♯[4]
+        @test scale[-3] == B[3]
     end
 
 
